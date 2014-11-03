f48b8876d6f968cd4d63eb5c3f7ba0bdf576622c
matiboy <mathieu.chauvinc@gmail.com>
2014-11-03 10:04:19 +0800

    We're not putting new url patterns at the top

--- test/output/added_namespace.py
+++ test/output/added_namespace.py
@@ -4,8 +4,8 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
+    url(r'^chicken/', include('apps.harlo.urls', namespace='blop')),
     # Root URL
     url(r'^/?$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^account/', include('apps.accounts.urls', namespace='account')),
-    url(r'^chicken/', include('apps.harlo.urls', namespace='blop')),
 )
