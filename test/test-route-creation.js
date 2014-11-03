118f933313b8a469a45c1dc29edba7dc614be622
matiboy <mathieu.chauvinc@gmail.com>
2014-11-03 12:26:29 +0800

    Tests for route creation in apps url

--- test/test-route-creation.js
+++ test/test-route-creation.js
@@ -14,11 +14,11 @@ describe('django route generator', function () {
             assert.throws(function createApp() {
                 this.app = helpers.createGenerator('django:route', [
                     '../../route'
-                ])
+                ]);
             });
         });
     });
-    describe('namespacing', function() {
+    describe('main urls and namespacing', function() {
         beforeEach(function (done) {
             var folder = path.join(__dirname, 'temp');
             helpers.testDirectory(folder, function (err) {
@@ -69,6 +69,71 @@ describe('django route generator', function () {
                 assert.equal(fs.readFileSync('../output/added_namespace_no_subdirectory.py', {encoding:'utf8'}),fs.readFileSync('urls.py', {encoding:'utf8'}))
                 done();
             });
+        });
+    });
+
+    describe('app urls', function() {
+        beforeEach(function (done) {
+            var folder = path.join(__dirname, 'temp');
+            helpers.testDirectory(folder, function (err) {
+                if (err) {
+                    return done(err);
+                }
+
+                // Copy fresh test urls to
+                // fs.mkdirSync('apps');
+                // fs.mkdirSync('apps/accounts');
+                fs.copySync(inputUrls, 'urls.py');
+
+                fs.copySync('../input/app_urls.py', 'apps/accounts/urls.py');
+
+                this.app = helpers.createGenerator('django:route', [
+                    '../../route'
+                ], ['accounts']);
+                done();
+            }.bind(this));
+        });
+        it('should create the urls file if it doesn t exist', function(done) {
+            helpers.mockPrompt(this.app, {
+                url: '^profile/(?P<id>\\d+)$',
+                viewClass: 'OtherAccount',
+                urlName: 'other_account'
+            });
+            this.app.run({}, function () {
+                var expected = ['apps/accounts/urls.py'];
+                done();
+            });
+        });
+        it('should add the correct line into urls.py without login required', function(done) {
+            helpers.mockPrompt(this.app, {
+                url: '^profile/(?P<id>\\d+)$',
+                viewClass: 'OtherAccount',
+                urlName: 'other_account'
+            });
+            this.app.run({}, function () {
+                assert.equal(fs.readFileSync('../output/added_url_no_login.py', {encoding:'utf8'}),fs.readFileSync('apps/accounts/urls.py', {encoding:'utf8'}))
+                done();
+            });
+        });
+        it('should add the correct line into urls.py with login required', function(done) {
+            helpers.mockPrompt(this.app, {
+                url: '^profile/(?P<id>\\d+)$',
+                viewClass: 'OtherAccount',
+                urlName: 'other_account'
+            });
+            this.app.run({}, function () {
+                helpers.mockPrompt(this.app, {
+                    url: '^(?P<id>\\d+)$',
+                    viewClass: 'MyFinances',
+                    urlName: 'my_finances',
+                    requiresLogin: true
+                });
+                this.app.run({}, function () {
+
+                    assert.equal(fs.readFileSync('../output/added_two_urls_one_with_login.py', {encoding:'utf8'}),fs.readFileSync('apps/accounts/urls.py', {encoding:'utf8'}))
+                    done();
+                });
+            }.bind(this));
         });
     });
 });
