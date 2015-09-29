a381cf072079fa066e5b4104358c2eabe8fb470a
Karl Birch <mbwkarl@gmail.com>
2015-09-29 10:35:35 +0800

    Add shell_plus alias and improve runserver task

--- app/templates/tasks/local/server.py
+++ app/templates/tasks/local/server.py
@@ -5,17 +5,16 @@ import os
 
 class Server(fabric.tasks.Task):
   name = 'server'
+  command = 'runserver'
+
   def run(self, port=8000, ip='127.0.0.1'):
-    fabric.api.local("python manage.py runserver --settings='{}' {}:{}".format(fabric.api.env.settings, ip, port))
+    fabric.api.local("python manage.py {} --settings='{}' {}:{}".format(self.command, fabric.api.env.settings, ip, port))
 
 class ServerAlias(Server):
   name = 'ls'
+  command = 'runserver'
 
-class ServerPlus(fabric.tasks.Task):
-  name = 'server'
-  def run(self, port=8000, ip='127.0.0.1'):
-    fabric.api.local("python manage.py runserver_plus --settings='{}' {}:{}".format(fabric.api.env.settings, ip, port))
-
-class ServerPlusAlias(ServerPlus):
+class ServerPlusAlias(Server):
   name = 'lsp'
+  command = 'runserver_plus'
 
--- app/templates/tasks/local/shell.py
+++ app/templates/tasks/local/shell.py
@@ -5,8 +5,16 @@ import os
 
 class Shell(fabric.tasks.Task):
   name = 'shell'
+  command = 'shell'
+
   def run(self):
-    fabric.api.local("python manage.py shell --settings='{}'".format(fabric.api.env.settings))
+    fabric.api.local("python manage.py {} --settings='{}'".format(self.command, fabric.api.env.settings))
 
 class ShellAlias(Shell):
   name = 'lsh'
+  command = 'shell'
+
+class ShellPlusAlias(Shell):
+  name = 'lshp'
+  command = 'shell_plus'
+
